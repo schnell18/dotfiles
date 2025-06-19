3b50f479adc01ddbfc02d33a1bd0c7c89323bcec
Justin Zhang <schnell18@gmail.com>
2025-06-19 18:27:06 +1200

    Fix nvim setup issues
    - Avoid node reinstall
    - Initialize nvim submodule and enable bash lsp server auto-install
    
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- setup.sh
+++ setup.sh
@@ -87,14 +87,16 @@ function setup_lua {
 }
 
 function setup_node {
-    # Download and install nvm:
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    if ! command -v node; then
+        # Download and install nvm:
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
-    # in lieu of restarting the shell
-    . "$HOME/.nvm/nvm.sh"
+        # in lieu of restarting the shell
+        . "$HOME/.nvm/nvm.sh"
 
-    # Download and install Node.js:
-    nvm install 22
+        # Download and install Node.js:
+        nvm install 22
+    fi
 }
 
 function setup_git {
@@ -169,6 +171,9 @@ function setup_nvim {
             exit 1
         ;;
     esac
+    git submodule init
+    git submodule update
+    (cd ./nvim/.config/nvim/ && git submodule init && git submodule update)
 }
 
 function setup_python {
