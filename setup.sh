db8580776f472652c0f1dec1581e7abe2f72cc15
Justin Zhang <schnell18@gmail.com>
2025-06-30 00:33:52 +1200

    Enable markdown preview
    - Add markdown preview plugin
    - Move go tools installation to dotfiles' setup.sh
    
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- setup.sh
+++ setup.sh
@@ -64,6 +64,23 @@ function setup_golang {
         bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
     fi
 
+    # delve
+    go install github.com/go-delve/delve/cmd/dlv@latest
+    # gofumpt
+    go install mvdan.cc/gofumpt@latest
+    # goimports
+    go install golang.org/x/tools/cmd/goimports@latest
+    # goimports-reviser
+    go install -v github.com/incu6us/goimports-reviser/v3@latest
+    # golines
+    go install github.com/segmentio/golines@latest
+    # gomodifytags
+    go install github.com/fatih/gomodifytags@latest
+    # gotests
+    go install github.com/cweill/gotests/gotests@latest
+    # gotestsum
+    go install gotest.tools/gotestsum@latest
+
     # gvm install go1.23
     # gvm use go1.23 --default
 }
