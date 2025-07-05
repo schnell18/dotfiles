df2884794721d8363a8676e3081c3cfa51ca3fab
Justin Zhang <schnell18@gmail.com>
2025-07-05 14:26:58 +1200

    Enable bash-completion for git
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- lib/git.sh
+++ lib/git.sh
@@ -1,6 +1,22 @@
 function __dotfiles_setup_git {
     echo "Setting up git"
-    # The rest of function is intentionally left blank.
+
+    # Install git and bash-completion
+    case $OS_DISTRO in
+        macos)
+            ensure_install git bash-completion
+        ;;
+        ubuntu)
+            ensure_install git bash-completion
+        ;;
+        arch)
+            ensure_install git bash-completion
+        ;;
+        *)
+            echo "Unsupported OS DISTRO: $OS_DISTRO" 1>&2
+            exit 1
+        ;;
+    esac
 }
 
 
