c8052e8e2e2e860511e9e5d8f1f3a66d45a1659b
Justin Zhang <schnell18@gmail.com>
2025-08-11 18:53:18 +1200

    Make go utils installation idempotent
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- lib/golang.sh
+++ lib/golang.sh
@@ -38,21 +38,69 @@ function __dotfiles_setup_golang {
     fi
 
     # delve
-    go install github.com/go-delve/delve/cmd/dlv@latest
-    # gofumpt
-    go install mvdan.cc/gofumpt@latest
-    # goimports
-    go install golang.org/x/tools/cmd/goimports@latest
-    # goimports-reviser
-    go install -v github.com/incu6us/goimports-reviser/v3@latest
-    # golines
-    go install github.com/segmentio/golines@latest
-    # gomodifytags
-    go install github.com/fatih/gomodifytags@latest
-    # gotests
-    go install github.com/cweill/gotests/gotests@latest
-    # gotestsum
-    go install gotest.tools/gotestsum@latest
+    ensure_go_cmd_installed \
+        dlv \
+        "github.com/go-delve/delve/cmd/dlv@latest"
 
+    # gofumpt
+    ensure_go_cmd_installed \
+        gofumpt \
+        "mvdan.cc/gofumpt@latest"
+
+    # goimports
+    ensure_go_cmd_installed \
+        goimports \
+        "golang.org/x/tools/cmd/goimports@latest"
+
+    # goimports-reviser
+    ensure_go_cmd_installed \
+        goimports-reviser \
+        "github.com/incu6us/goimports-reviser/v3@latest"
+
+    # golines
+    ensure_go_cmd_installed \
+        golines \
+        "github.com/segmentio/golines@latest"
+
+    # gomodifytags
+    ensure_go_cmd_installed \
+        gomodifytags \
+        "github.com/fatih/gomodifytags@latest"
+
+    # gotests
+    ensure_go_cmd_installed \
+        gotests \
+        "github.com/cweill/gotests/gotests@latest"
+
+    # gotestsum
+    ensure_go_cmd_installed \
+        gotestsum \
+        "gotest.tools/gotestsum@latest"
+
+    # setup PATH
+    ensure_go_utils_in_path
+}
+
+ensure_go_cmd_installed() {
+    cmd_name=$1
+    cmd_url=$2
+
+    if ! command -v $cmd_name &>/dev/null; then
+        echo "Setup ${cmd_name}..."
+        go install $cmd_url
+    fi
+}
+
+ensure_go_utils_in_path() {
+  if ! grep -q 'export PATH="$HOME/go/bin:$PATH"' "$HOME/.bashrc" 2>/dev/null; then
+    echo 'export PATH="$HOME/go/bin:$PATH"' >>"$HOME/.bashrc"
+    echo "Added go utils to PATH in ~/.bashrc"
+    echo "Relaunch bash to take effect!!!"
+  fi
+  if ! grep -q 'export PATH="$HOME/go/bin:$PATH"' "$HOME/.zshrc" 2>/dev/null; then
+    echo 'export PATH="$HOME/go/bin:$PATH"' >>"$HOME/.zshrc"
+    echo "Added go utils to PATH in ~/.zshrc"
+    echo "Relaunch zsh to take effect!!!"
+  fi
 }
 
